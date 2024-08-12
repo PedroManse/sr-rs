e081850023126f3165473a3acfc5fe6bc4c6d5dd
Pedro Manse <pedro.manse@dmk3.com.br>
2024-08-12 16:57:56 -0300

    sqlx: migration for notebook calendar, notes & groups

---
+++ migrations/20240812185227_notebook.down.sql
@@ -0,0 +1,12 @@
+-- Add down migration script here
+DROP INDEX notebook.notebook_user_calendar_entries;
+DROP INDEX notebook.notebook_user_notes;
+DROP INDEX notebook.notebook_group_calendar_entries;
+
+DROP TABLE notebook.group_calendar_entries;
+DROP TABLE notebook.group_users;
+DROP TABLE notebook.groups;
+
+DROP TABLE notebook.user_calendar_entries;
+DROP TABLE notebook.notes;
+DROP SCHEMA "notebook";
---
+++ migrations/20240812185227_notebook.up.sql
@@ -0,0 +1,37 @@
+-- Add up migration script here
+CREATE SCHEMA "notebook";
+
+CREATE TABLE notebook.groups (
+	id UUID NOT NULL PRIMARY KEY DEFAULT gen_random_uuid(),
+	owner_id UUID NOT NULL REFERENCES inter.accounts(id),
+	name TEXT NOT NULL UNIQUE,
+	description TEXT
+);
+
+CREATE TABLE notebook.user_calendar_entries (
+	owner_id UUID NOT NULL REFERENCES inter.accounts(id),
+	time DATE NOT NULL,
+	title TEXT NOT NULL,
+	description TEXT
+);
+CREATE INDEX notebook_user_calendar_entries ON notebook.user_calendar_entries(owner_id);
+
+CREATE TABLE notebook.notes (
+	owner_id UUID NOT NULL REFERENCES inter.accounts(id),
+	content TEXT NOT NULL
+);
+CREATE INDEX notebook_user_notes ON notebook.notes(owner_id);
+
+CREATE TABLE notebook.group_users (
+	group_id UUID NOT NULL REFERENCES notebook.groups(id),
+	user_id UUID NOT NULL REFERENCES inter.accounts(id)
+);
+
+CREATE TABLE notebook.group_calendar_entries (
+	group_id UUID NOT NULL REFERENCES notebook.groups(id),
+	time DATE NOT NULL,
+	title TEXT NOT NULL,
+	description TEXT
+);
+CREATE INDEX notebook_group_calendar_entries ON notebook.group_calendar_entries(group_id);
+
